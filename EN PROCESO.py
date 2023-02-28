50bedc829953c310a20d15f4f6ce59b75740e225
iamfrans <1323544@senati.pe>
2023-02-28 11:53:33 -0500

    cuadro

--- EN PROCESO.py
+++ EN PROCESO.py
@@ -9,6 +9,13 @@ class Product:
         self.wind = window 
 #Le damos el titulo 
         self.wind.title('Ferreteria: El baul de Anonymous')
+ #creando cuadro para agregar elemnto 
+        DNI = LabelFrame(self.wind, text='')
+        DNI.grit(row = 0 columm = 0, columnspan = 3, pady = 28)
+
+        label(frame, Text = 'DNI: ').grit(row = 1, column = 0)
+        self.name = Entry(Frame)
+        self.name.grid(row = 1, column =1)
 #ventana de Aplicativo
 if __name__ == '_main_':
     window = Tk()
