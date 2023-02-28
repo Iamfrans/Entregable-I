8644d6ea7c0fc083b95a879ab395e979c13c404e
iamfrans <1323544@senati.pe>
2023-02-28 11:03:21 -0500

    class Product

--- EN PROCESO.py
+++ EN PROCESO.py
@@ -1,3 +1,9 @@
 #diseño de interfaz
 from tkinter import ttkthemes
 from tkinter import *
+
+import sqlite3
+#creamos class product para agregar, eliminar, editar productos
+class Product:
+    def __init__(self, window):
+        self.wind = window 
